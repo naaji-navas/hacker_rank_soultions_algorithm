128250d43852ce09c4b90f5623af9e2d6461349c
naaji-navas <najidnavas2000@gmail.com>
2023-10-19 19:02:34 +0530

    Inital commit

--- SolveMeFirst.java
+++ SolveMeFirst.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class SolveMeFirst {
 
     // Complete the aVeryBigSum function below.
     static long aVeryBigSum(long[] ar) {
